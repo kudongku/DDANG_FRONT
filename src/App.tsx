30b07275e0fc4391d905610c4f8be4263b215e96
peter <peter@soulware.kr>
2025-01-21 15:43:48 +0900

    feat: react-router-dom 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello, world</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
