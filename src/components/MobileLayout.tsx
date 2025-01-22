4e31ba5b9a493c23b0b10af691f45d11bdf32de4
peter <peter@soulware.kr>
2025-01-22 14:36:11 +0900

    feat: mobile layout 적용

---
+++ src/components/MobileLayout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+interface MobileLayoutProps {
+  children: ReactNode;
+}
+
+export default function MobileLayout({ children }: MobileLayoutProps) {
+  return (
+    <div className="antialiased max-w-[430px] mx-auto h-screen overflow-y-auto bg-blue-100">
+      {children}
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import MobileLayout from "./components/MobileLayout.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MobileLayout>
+      <App />
+    </MobileLayout>
   </StrictMode>
 );
