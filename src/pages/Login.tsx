5858ceddd7c7b120af475a9c5cbd649144f5523d
peter <peter@soulware.kr>
2025-01-24 14:05:08 +0900

    feat: 로그인 기능 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,81 @@
+import axios from "axios";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    // 회원가입 페이지로 이동하기
-    navigate("/signup");
+  const [error, setError] = useState("");
+  const [emailValidation, setEmailValidation] = useState(false);
+  const [formdata, setFormdata] = useState({
+    email: "",
+    password: "",
+  });
+
+  const validateEmail = (email: string) =>
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    if (name === "email") {
+      setEmailValidation(validateEmail(value));
+    }
+
+    setFormdata((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8080/api/auth/login", formdata)
+      .then(({ data }) => {
+        localStorage.setItem("accessToken", data.tokenType + data.token);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.response?.data || "로그인 실패");
+      });
   };
 
   return (
     <>
       <h1>login page</h1>
-      <button onClick={handleClick}>회원가입 하러 가기</button>
+
+      <form>
+        <div>
+          <label>
+            email :
+            <br />
+            <input type="text" name="email" onChange={handleChange} />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            password :
+            <br />
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              disabled={!emailValidation}
+            />
+          </label>
+        </div>
+
+        {error && <p style={{ color: "red" }}>{error}</p>}
+
+        <button onClick={handleSubmit} disabled={!emailValidation}>
+          제출하기
+        </button>
+      </form>
+
+      <button onClick={() => navigate("/signup")}>회원가입 하러 가기</button>
     </>
   );
 }
