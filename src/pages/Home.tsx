2cbedbfd5b207be6c09edf572812084f5cd51f4e
peter <peter@soulware.kr>
2025-02-06 22:24:12 +0900

    feat: 메인페이지 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+import api from "../apis/api";
+
 export default function Home() {
-  return <h1>hello, world</h1>;
+  const [userMail, setUserMail] = useState("");
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await api.get("users/info");
+        console.log(response.data); // ✅ 실제 데이터를 출력
+        setUserMail(response.data);
+      } catch (error) {
+        console.error("API 요청 실패:", error);
+      }
+    };
+
+    fetchUserInfo(); // 함수 실행
+  }, [userMail]); // ✅ 빈 배열을 넣어 한 번만 실행되도록 설정
+
+  return <h1>hello, world {userMail}</h1>;
 }
