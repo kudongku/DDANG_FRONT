0b3198f8d62138a05b307c2a2386454afc6d1c72
peter <peter@soulware.kr>
2025-02-16 23:25:01 +0900

    refactor: hook 사용으로 redux module 최적화

--- src/components/CreateAuctionContainer.tsx
+++ src/components/CreateAuctionContainer.tsx
@@ -1,25 +1,38 @@
-import { connect } from 'react-redux';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useSelector, useDispatch } from 'react-redux';
 import { changeTitle, changeContent } from '../modules/auctionCreaterModules';
 import CreateAuction from './CreateAuction';
 import { useNavigate } from 'react-router-dom';
 import { createAuctionApi } from '../apis/auctions';
+import { useCallback } from 'react';
 
 /**
  * container component
  * (데이터를 전달하고 이벤트를 처리하는 컴포넌트)
  */
-const CreateAuctionContainer = ({
-  title,
-  content,
-  onChangeTitle,
-  onChangeContent,
-}: {
-  title: string;
-  content: string;
-  onChangeTitle: (title: string) => void;
-  onChangeContent: (content: string) => void;
-}) => {
+const CreateAuctionContainer = () => {
   const navigate = useNavigate();
+
+  const title = useSelector(
+    (state: { auctionCreater: { title: string } }) => state.auctionCreater.title
+  );
+  const content = useSelector(
+    (state: { auctionCreater: { content: string } }) => state.auctionCreater.content
+  );
+
+  const dispatch = useDispatch();
+  const onChangeTitle = useCallback(
+    (title: string) => {
+      dispatch(changeTitle(title) as any);
+    },
+    [dispatch]
+  );
+  const onChangeContent = useCallback(
+    (content: string) => {
+      dispatch(changeContent(content) as any);
+    },
+    [dispatch]
+  );
 
   const handleCreateAuction = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -47,26 +60,4 @@ const CreateAuctionContainer = ({
   );
 };
 
-/**
- * mapStateToProps : 리덕스 스토어의 상태를 컴포넌트의 프롭스로 전달하는 함수
- * (리덕스 스토어의 상태를 컴포넌트의 프롭스로 전달)
- */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const mapStateToProps = (state: any) => ({
-  title: state.auctionCreater.title,
-  content: state.auctionCreater.content,
-});
-
-/**
- * mapDispatchToProps : 리덕스 스토어의 액션을 컴포넌트의 이벤트로 전달하는 함수
- * (리덕스 스토어의 액션을 컴포넌트의 이벤트로 전달)
- */
-const mapDispatchToProps = {
-  onChangeTitle: changeTitle,
-  onChangeContent: changeContent,
-};
-/**
- * connect : 리덕스 스토어와 컴포넌트를 연결하는 함수
- * (리덕스 스토어의 상태를 컴포넌트의 프롭스로 전달)
- */
-export default connect(mapStateToProps, mapDispatchToProps)(CreateAuctionContainer);
+export default CreateAuctionContainer;
